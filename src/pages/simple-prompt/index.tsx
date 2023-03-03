3020986a0e3edaa2c3eb2538a4042b0bab16c60e
Noah Milberger <n.milberger@gmail.com>
2023-03-03 11:43:58 -0500

    Added custom TextField.

--- src/pages/simple-prompt/index.tsx
+++ src/pages/simple-prompt/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, useForm, UseFormSetValue } from "react-hook-form";
 import Page from "@/components/ui/page/Page";
+import TextField from "@/components/ui/form-fields/TextField";
 
 const title = `Simple Prompt`;
 
@@ -11,48 +12,70 @@ type DataType = {
   };
 };
 
+interface OnSubmitProps {
+  prompt: FieldValues;
+  setData: (data: DataType) => void;
+  setValue: UseFormSetValue<FieldValues>;
+  setLoading: (loading: boolean) => void;
+}
+
+// Submit function
+const onSubmit = async ({
+  prompt,
+  setData,
+  setValue,
+  setLoading,
+}: OnSubmitProps) => {
+  setLoading(true);
+  try {
+    const response = await fetch("api/simple-prompt-response", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(prompt),
+    });
+    console.log("response: ", response);
+    const data = await response.json();
+    console.log("data: ", data);
+    setData(data);
+    setLoading(false);
+  } catch (error) {
+    console.error("Error submitting prompt: ", error);
+    setLoading(false);
+  }
+};
+
 export default function SimplePrompt() {
-  const { register, handleSubmit } = useForm<FieldValues>(); // initalize for react-hook-form
+  // React Hook Form variables
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+  } = useForm<FieldValues>();
 
   // Variables
-  const [prompt, setPrompt] = useState("");
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<DataType>();
-
-  // Submit function
-  const onSubmit = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch("api/simple-prompt-response", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: prompt }),
-      });
-      console.log("response: ", response);
-      const data = await response.json();
-      setData(data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error submitting prompt: ", error);
-      setLoading(false);
-    }
-  };
 
   return (
     <Page>
       <div className="flex flex-col w-full justify-center items-center gap-12">
         <h1 className="text-4xl font-bold">{title}</h1>
         <form
-          onSubmit={handleSubmit(onSubmit)}
           className="w-1/2 flex flex-col gap-4 items-center"
+          onSubmit={handleSubmit((prompt) =>
+            onSubmit({ prompt, setData, setValue, setLoading })
+          )}
         >
-          <input
-            {...register("prompt")}
-            className="p-4 rounded-xl w-full text-main-light"
+          <TextField
+            control={control}
+            name="prompt"
+            type="text"
             placeholder="Enter a prompt"
-            onChange={(e) => setPrompt(e.target.value)}
+            required="Prompt is required."
+            errors={errors}
           />
           <input
             className="cursor-pointer border-2 w-fit py-2 px-4 rounded-full hover:scale-110"
